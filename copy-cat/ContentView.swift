0407ce9a37d646e7c7bdcee8ce1664ed052d7866
kevin12v <kevinvilcacab@gmail.com>
2024-06-16 12:01:10 -0500

    Initial Commit

---
+++ copy-cat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  copy-cat
+//
+//  Created by Kevin Vilca on 16/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ copy-cat/copy_catApp.swift
@@ -0,0 +1,17 @@
+//
+//  copy_catApp.swift
+//  copy-cat
+//
+//  Created by Kevin Vilca on 16/06/24.
+//
+
+import SwiftUI
+
+@main
+struct copy_catApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
